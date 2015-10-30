f5a1bd6a22bc976413cd1d74f42a5d8d4b017b0c
Chris Bee <chris@ctrlbee.com>
2015-10-29 17:57:58 -0700

    Refactored request handler to use a Promise based solution

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,16 +1,72 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-var httpHelpers = require('./http-helpers.js')
-// require more modules/folders here!
+var httpHelpers = require('./http-helpers.js');
+var fs = require('fs'); 
+var Promise = require('bluebird');
 
-// var routes = {
-//   '/': function (res) {
-//     httpHelpers.serveAssets(res, archive.paths.siteAssets + '/index.html', function (data) {
-//       res.end(data);
-//     })
-//   }
-// }
+///////////////////////////
+/////PROMISE SOLUTION//////
+///////////////////////////
 
+var readFile = Promise.promisify(fs.readFile); 
+
+exports.handleRequestPromise = function (req, res){
+  if(req.url==="/" && req.method ==='POST'){
+    //collect data from post and substring it
+    var sub = ""; 
+    collectData(req)
+      .then(function(data){
+        sub = data.substring(4); 
+        return archive.paths.archivedSites + "/" + sub; 
+      })
+      .then(function(file){
+         if(archive.isUrlArchived(file)){
+            return readFile(file); 
+         } else {
+            if(!archive.isUrlInList(sub)){
+              archive.addUrlToList(sub);   
+            }
+            return readFile(archive.paths.siteAssets + '/loading.html')  
+          }
+      })
+      .then(function(htmlFile){
+        httpHelpers.sendResponse(res, htmlFile); 
+      })
+      .catch(function(err){
+        console.log("error in the chain"); 
+      })
+  }
+
+
+   // SERVES UP INITIAL STATIC ASSETS
+  if (req.url === '/' && req.method === 'GET') {
+    httpHelpers.serveAssets(archive.paths.siteAssets + '/index.html', function (data) {
+      httpHelpers.sendResponse(res, data);
+    })
+  } else if (req.url === '/styles.css' && req.method === 'GET') {
+    httpHelpers.serveAssets(archive.paths.siteAssets + '/styles.css', function (data) {
+      httpHelpers.sendResponse(res, data);
+    })
+  }
+}
+
+//refeactored to return a promise (pulled from the http-helpers)
+var collectData = function(request) {
+  return new Promise(function(resolve, reject){
+    var data = "";
+    request.on('data', function(chunk) {
+      data += chunk;
+    });
+    request.on('end', function() {
+      resolve(data);
+    });
+  })
+};
+
+
+////////////////////////////
+////NON-PROMISE SOLUTION////
+////////////////////////////
 
 exports.handleRequest = function (req, res) {
   // SERVES HTML REQUESTED FROM FORM
